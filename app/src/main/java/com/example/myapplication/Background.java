b37ef3db59c87d7b274ed704ad84782a101ac08b
stronger4444 <khanhdohoangmai4444@gmail.com>
2021-10-29 20:08:43 +0700

    buoi4

--- app/src/main/java/com/example/myapplication/Background.java
+++ app/src/main/java/com/example/myapplication/Background.java
@@ -27,6 +27,8 @@ public class Background extends AppCompatActivity {
         imageViewPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Background.this,RegisterInformation.class);
+                startActivity(intent);
 
             }
         });
